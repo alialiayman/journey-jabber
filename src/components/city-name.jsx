fb0ef028a2918289682fdd447d52e1a28b76091e
AymanA <aliayman@alumni.harvard.edu>
2023-12-09 11:13:18 -0800

    firebase deploy

--- src/components/city-name.jsx
+++ src/components/city-name.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
-import { IconButton, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import announce, { stopAnnouncement } from "../shared/announce";
+import { RefreshOutlined } from "@mui/icons-material";
 
 const CityName = ({ cities }) => {
   const [isMuted, setIsMuted] = React.useState(true);
@@ -19,25 +20,33 @@ const CityName = ({ cities }) => {
     <Stack
       direction={"row"}
       alignItems={"center"}
+      justifyContent={"space-between"}
       sx={{
-        boxShadow: "0 4px 8px green",
+        boxShadow: "0 4px 8px hsl(120, 100%, 15%)",
         height: "100%",
         paddingLeft: "1rem",
+        borderRadius: "1rem",
+        backgroundColor: "hsl(120,100%,95%)",
       }}
     >
-      <Typography variant="h4" color="primary">
+      <Typography variant="h4" sx={{color: 'hsl(120,100%,10%)'}}>
         {cities && cities?.[0]?.[0]}
       </Typography>
+      <Box>
       {isMuted && (
         <IconButton onClick={() => handleSpeak(false)} color="primary">
-          <VolumeUpIcon />
+          <VolumeUpIcon sx={{color: 'hsl(120,100%,20%)'}}/>
         </IconButton>
       )}
       {!isMuted && (
-        <IconButton onClick={() => handleSpeak(true)} color="secondary">
-          <VolumeOffIcon />
+        <IconButton onClick={() => handleSpeak(true)} >
+          <VolumeOffIcon sx={{color: 'hsl(120,100%,20%)'}}/>
         </IconButton>
       )}
+      <IconButton >
+        <RefreshOutlined onClick={() => window.location.reload()} sx={{color: 'hsl(120,100%,20%)'}}/>
+      </IconButton>
+      </Box>
     </Stack>
   );
 };
